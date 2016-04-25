8986e703575a7c2467366ef0893ff7b7290e5f69
Jeff Williams <jeff_w123@hotmail.com>
2016-04-25 22:18:10 +1200

    test update

--- BattleShips/Ship/ShipPositions.cs
+++ BattleShips/Ship/ShipPositions.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace BattleShips
 {
     /// <summary>
-    /// This class stores the list of grid positions occupied by a ship.
+    /// This class stores the list of grid positions occupied by a ship. This also includes a boolean property called IsFloating that is used as a flag for positions hit by missiles.
     /// </summary>
     public class ShipPositions
     {
