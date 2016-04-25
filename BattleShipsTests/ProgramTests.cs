cbf601c6539f5c002bb56e62fd7e10dc7e9a4514
Jeff Williams <jeff_w123@hotmail.com>
2016-04-25 16:28:06 +1200

    added tests

---
+++ BattleShipsTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleShips;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShips.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
