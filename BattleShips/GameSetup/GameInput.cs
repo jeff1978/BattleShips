414e69d4bc22bd877c57d053019d8f13fa2c6a2f
Jeff Williams <jeff_w123@hotmail.com>
2016-04-28 18:55:07 +1200

    update

---
+++ BattleShips/GameSetup/GameInput.cs
@@ -0,0 +1,35 @@
+﻿using BattleShips.Ship;
+using System;
+using System.Collections.Generic;
+
+namespace BattleShips.Setup
+{
+    /// <summary>
+    /// This class is used to implement the methods 
+    /// for user inputs during game setup.
+    /// </summary>
+    public static class GameInput
+    {
+
+        public static List<ShipTypeInGame> getSettings(GameMode gameMode)
+        {
+            switch (gameMode)
+            {
+                case GameMode.Default:
+                    return GameDefault.setDefaults();
+                  
+                case GameMode.Custom:
+                    return GameCustom.setCustom();
+                   
+                 //   more cases can be added here in the future
+                 //   eg.a new game mode.
+
+                default:
+                    Console.WriteLine("The game cannot be initialised. No method implementation has been found for your selection.");
+                    Console.ReadLine();
+                    break;
+            }
+            return GameDefault.setDefaults();
+        }
+    }
+}
